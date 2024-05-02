4e0f38d841dd3437cb9e65cf18b4027f65bd8457
Danny Thuering <danny@webmobix.com>
2024-05-02 15:17:39 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Sanctum Link',
+  webDir: 'dist'
+};
+
+export default config;
